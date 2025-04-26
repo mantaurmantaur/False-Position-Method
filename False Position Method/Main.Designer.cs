74efe2c6dcbd9fa2f726cddefbf560aae8786c7f
mantaurmantaur <orng.digital@gmail.com>
2025-04-26 16:43:28 +0800

    Add files via upload

---
+++ False Position Method/Main.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace False_Position_Method
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.label1 = new System.Windows.Forms.Label();
+            this.controlsPanel = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.output = new System.Windows.Forms.RichTextBox();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.calculateBtn = new System.Windows.Forms.Button();
+            this.txtTolerance = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEquation = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.controlsPanel.SuspendLayout();
+            this.mainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(55)))), ((int)(((byte)(89)))));
+            this.label1.Location = new System.Drawing.Point(227, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(474, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FALSE POSITION METHOD";
+            // 
+            // controlsPanel
+            // 
+            this.controlsPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(179)))), ((int)(((byte)(205)))), ((int)(((byte)(224)))));
+            this.controlsPanel.Controls.Add(this.button2);
+            this.controlsPanel.Controls.Add(this.button1);
+            this.controlsPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.controlsPanel.Location = new System.Drawing.Point(0, 0);
+            this.controlsPanel.Name = "controlsPanel";
+            this.controlsPanel.Size = new System.Drawing.Size(898, 30);
+            this.controlsPanel.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Image = global::False_Position_Method.Properties.Resources.cross;
+            this.button2.Location = new System.Drawing.Point(857, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(38, 27);
+            this.button2.TabIndex = 4;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = global::False_Position_Method.Properties.Resources.minus;
+            this.button1.Location = new System.Drawing.Point(813, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(38, 27);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(179)))), ((int)(((byte)(205)))), ((int)(((byte)(224)))));
+            this.mainPanel.Controls.Add(this.output);
+            this.mainPanel.Controls.Add(this.chart2);
+            this.mainPanel.Controls.Add(this.calculateBtn);
+            this.mainPanel.Controls.Add(this.txtTolerance);
+            this.mainPanel.Controls.Add(this.label4);
+            this.mainPanel.Controls.Add(this.txtB);
+            this.mainPanel.Controls.Add(this.txtA);
+            this.mainPanel.Controls.Add(this.label3);
+            this.mainPanel.Controls.Add(this.label2);
+            this.mainPanel.Controls.Add(this.label5);
+            this.mainPanel.Controls.Add(this.txtEquation);
+            this.mainPanel.Location = new System.Drawing.Point(0, 108);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(898, 544);
+            this.mainPanel.TabIndex = 3;
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(12, 190);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(410, 324);
+            this.output.TabIndex = 19;
+            this.output.Text = "";
+            this.output.WordWrap = false;
+            // 
+            // chart2
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart2.Legends.Add(legend1);
+            this.chart2.Location = new System.Drawing.Point(428, 190);
+            this.chart2.Name = "chart2";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart2.Series.Add(series1);
+            this.chart2.Size = new System.Drawing.Size(458, 324);
+            this.chart2.TabIndex = 17;
+            this.chart2.Text = "chart2";
+            // 
+            // calculateBtn
+            // 
+            this.calculateBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(31)))), ((int)(((byte)(75)))));
+            this.calculateBtn.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateBtn.ForeColor = System.Drawing.Color.White;
+            this.calculateBtn.Location = new System.Drawing.Point(711, 101);
+            this.calculateBtn.Name = "calculateBtn";
+            this.calculateBtn.Size = new System.Drawing.Size(140, 56);
+            this.calculateBtn.TabIndex = 1;
+            this.calculateBtn.Text = "Solve";
+            this.calculateBtn.UseVisualStyleBackColor = false;
+            this.calculateBtn.Click += new System.EventHandler(this.calculateBtn_Click);
+            // 
+            // txtTolerance
+            // 
+            this.txtTolerance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(31)))), ((int)(((byte)(75)))));
+            this.txtTolerance.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTolerance.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTolerance.ForeColor = System.Drawing.Color.White;
+            this.txtTolerance.Location = new System.Drawing.Point(508, 122);
+            this.txtTolerance.Multiline = true;
+            this.txtTolerance.Name = "txtTolerance";
+            this.txtTolerance.Size = new System.Drawing.Size(140, 35);
+            this.txtTolerance.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(504, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 23);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Es";
+            // 
+            // txtB
+            // 
+            this.txtB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(31)))), ((int)(((byte)(75)))));
+            this.txtB.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtB.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtB.ForeColor = System.Drawing.Color.White;
+            this.txtB.Location = new System.Drawing.Point(293, 122);
+            this.txtB.Multiline = true;
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 35);
+            this.txtB.TabIndex = 13;
+            // 
+            // txtA
+            // 
+            this.txtA.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(31)))), ((int)(((byte)(75)))));
+            this.txtA.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtA.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtA.ForeColor = System.Drawing.Color.White;
+            this.txtA.Location = new System.Drawing.Point(78, 122);
+            this.txtA.Multiline = true;
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 35);
+            this.txtA.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(289, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 23);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Lower Boundary";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(77, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 23);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Upper Boundary";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(102, 6);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(129, 23);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Enter Equation";
+            // 
+            // txtEquation
+            // 
+            this.txtEquation.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(31)))), ((int)(((byte)(75)))));
+            this.txtEquation.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtEquation.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEquation.ForeColor = System.Drawing.Color.White;
+            this.txtEquation.Location = new System.Drawing.Point(106, 39);
+            this.txtEquation.Multiline = true;
+            this.txtEquation.Name = "txtEquation";
+            this.txtEquation.Size = new System.Drawing.Size(672, 42);
+            this.txtEquation.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(178, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(58, 60);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(898, 649);
+            this.ControlBox = false;
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.controlsPanel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Main";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.controlsPanel.ResumeLayout(false);
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel controlsPanel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Button calculateBtn;
+        private System.Windows.Forms.TextBox txtTolerance;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEquation;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.RichTextBox output;
+    }
+}
+
---
+++ False Position Method/Main.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using NCalc;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace False_Position_Method
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+
+        private double Evaluate(string expression, double x)
+        {
+            Expression expr = new Expression(expression);
+            expr.Parameters["x"] = x;
+            return Convert.ToDouble(expr.Evaluate());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void calculateBtn_Click(object sender, EventArgs e)
+        {
+            output.Clear();
+            chart2.Series.Clear();
+
+            string rawInput = txtEquation.Text.Trim();
+            string equationInput = ConvertExponents(rawInput);
+
+            if (!double.TryParse(txtA.Text, out double a) ||
+                !double.TryParse(txtB.Text, out double b) ||
+                !double.TryParse(txtTolerance.Text, out double tolerance))
+            {
+                MessageBox.Show("Please enter valid numbers for a, b, and tolerance.");
+                return;
+            }
+
+            double fa = Evaluate(equationInput, a);
+            double fb = Evaluate(equationInput, b);
+
+            if (fa * fb > 0)
+            {
+                MessageBox.Show("Function has same signs at a and b. Try different values.");
+                return;
+            }
+
+            InitializeChart(a, b);
+
+            Series funcSeries = CreateFunctionSeries(equationInput, a, b);
+            chart2.Series.Add(funcSeries);
+
+            Series zeroLine = CreateZeroLine(a, b);
+            chart2.Series.Add(zeroLine);
+
+            Series rootSeries = CreateRootSeries();
+            chart2.Series.Add(rootSeries);
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Iter\t a\t b\t c\t fc \tError");
+
+            double c = a;
+            double fc = 0;
+            double prevC = a;
+            double error = double.MaxValue;
+            int iter = 0;
+
+            int maxIter = 100;
+            int stuckCounter = 0;
+            while (iter < maxIter)
+            {
+                prevC = c;
+                c = (a * fb - b * fa) / (fb - fa);
+                fc = Evaluate(equationInput, c);
+
+                iter++;
+
+                if (iter > 1)
+                {
+                    error = Math.Abs((c - prevC) / c) * 100;
+                    sb.AppendLine($"{iter,-4}{a,10:F6}{b,10:F6}{c,10:F6}{fc,10:F6}{error,10:F2}%");
+
+                    if (error <= tolerance)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    sb.AppendLine($"{iter,-4}{a,10:F6}{b,10:F6}{c,10:F6}{fc,10:F6}{"-",10}");
+                }
+
+                rootSeries.Points.Clear();
+                rootSeries.Points.AddXY(c, 0);
+                Application.DoEvents();
+
+                if (fa * fb < 0)
+                {
+                    b = c;
+                    fb = fc;
+                    stuckCounter = 0;
+                }
+                else
+                {
+                    a = c;
+                    fa = fc;
+                    stuckCounter++;
+
+                    if (stuckCounter >= 2)
+                    {
+                        fb /= 2;
+                    }
+                }
+            }
+
+            sb.AppendLine($"\nRoot found: x = {c:F6} after {iter} iterations");
+            output.Text = sb.ToString();
+
+            rootSeries.MarkerSize = 12;
+            rootSeries.Color = Color.Green;
+            rootSeries.Points[0].Color = Color.Green;
+        }
+
+        private void InitializeChart(double a, double b)
+        {
+            var chartArea = chart2.ChartAreas[0];
+            chartArea.AxisX.Title = "x";
+            chartArea.AxisY.Title = "y";
+            chartArea.AxisX.Crossing = 0;
+            chartArea.AxisY.Crossing = 0;
+            chartArea.AxisX.IsMarginVisible = false;
+            chartArea.AxisY.IsMarginVisible = false;
+            chartArea.AxisX.MajorGrid.LineColor = Color.LightGray;
+            chartArea.AxisY.MajorGrid.LineColor = Color.LightGray;
+            chartArea.AxisX.Minimum = a - 1;
+            chartArea.AxisX.Maximum = b + 1;
+        }
+
+        private Series CreateFunctionSeries(string equation, double a, double b)
+        {
+            Series funcSeries = new Series("f(x)")
+            {
+                ChartType = SeriesChartType.Line,
+                Color = Color.Red,
+                BorderWidth = 2
+            };
+
+            for (double x = a - 1; x <= b + 1; x += 0.1)
+            {
+                double y = Evaluate(equation, x);
+                funcSeries.Points.AddXY(x, y);
+            }
+
+            double yMin = funcSeries.Points.Min(p => p.YValues[0]);
+            double yMax = funcSeries.Points.Max(p => p.YValues[0]);
+            chart2.ChartAreas[0].AxisY.Minimum = Math.Floor(yMin) - 1;
+            chart2.ChartAreas[0].AxisY.Maximum = Math.Ceiling(yMax) + 1;
+
+            return funcSeries;
+        }
+
+        private Series CreateZeroLine(double a, double b)
+        {
+            Series zeroLine = new Series("y=0")
+            {
+                ChartType = SeriesChartType.Line,
+                Color = Color.Gray,
+                BorderWidth = 1,
+                BorderDashStyle = ChartDashStyle.Dash
+            };
+            zeroLine.Points.AddXY(a - 1, 0);
+            zeroLine.Points.AddXY(b + 1, 0);
+            return zeroLine;
+        }
+
+        private Series CreateRootSeries()
+        {
+            return new Series("Root")
+            {
+                ChartType = SeriesChartType.Point,
+                MarkerStyle = MarkerStyle.Circle,
+                MarkerSize = 8,
+                Color = Color.Blue
+            };
+        }
+
+        private string ConvertExponents(string input)
+        {
+            input = Regex.Replace(input, @"(?<num>\d)(?=[a-zA-Z(])", "${num}*");
+            input = Regex.Replace(input, @"(?<var>[a-zA-Z])(?=\()", "${var}*");
+            input = Regex.Replace(input, @"(?<=\))(?=\()", ")*(");
+            input = Regex.Replace(input, @"(?<=\))(?=[a-zA-Z])", ")*");
+
+            // Fix function names for NCalc
+            input = Regex.Replace(input, @"\bsin\b", "Sin", RegexOptions.IgnoreCase);
+            input = Regex.Replace(input, @"\bcos\b", "Cos", RegexOptions.IgnoreCase);
+            input = Regex.Replace(input, @"\btan\b", "Tan", RegexOptions.IgnoreCase);
+            input = Regex.Replace(input, @"\blog\b", "Log", RegexOptions.IgnoreCase);
+            input = Regex.Replace(input, @"\bsqrt\b", "Sqrt", RegexOptions.IgnoreCase);
+            input = Regex.Replace(input, @"\bexp\b", "Exp", RegexOptions.IgnoreCase);
+            input = Regex.Replace(input, @"\babs\b", "Abs", RegexOptions.IgnoreCase);
+
+            // Handle exponents like x^3
+            string pattern = @"(?<base>(\([^()]+\)|[a-zA-Z0-9_\.]+))\s*\^\s*(?<exp>-?\d+)";
+            input = Regex.Replace(input, pattern, m =>
+            {
+                string baseExpr = m.Groups["base"].Value;
+                string exponent = m.Groups["exp"].Value;
+                return $"Pow({baseExpr},{exponent})";
+            });
+
+            return input;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
